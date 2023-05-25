7f98f50678d888421c3ca861995d4f826c22216d
ibrahim3999 <mgdlhb@gmail.com>
2023-05-25 21:25:44 +0300

    Prime.c & Queue.c

---
+++ Prime.c
@@ -0,0 +1,18 @@
+#include <math.h>
+
+int isPrime(unsigned int number) {
+    if (number < 2) {
+        return 0; 
+    }
+    
+    unsigned int sqrtNumber = sqrt(number);
+    for (unsigned int i = 2; i <= sqrtNumber; i++) {
+        if (number % i == 0) {
+            return 0; 
+        }
+    }
+    
+    return 1;  
+}
+
+

8b0c6e9901c073a5610902ba195152922d52cb5d
Jameely <119011959+Jameely@users.noreply.github.com>
2023-05-26 23:17:13 +0300

    Update pipelie.c

--- pipelie.c
+++ pipelie.c
@@ -3,22 +3,23 @@
 #include <pthread.h>
 #include <time.h>
 #include <math.h>
+#include <unistd.h>
 
 #define MAX_QUEUE_SIZE 10
 
 int isPrime(unsigned int number) {
     if (number < 2) {
-        return 0; 
+        return 0;
     }
-    
+
     double sqrtNumber = sqrt((double)number);
     for (unsigned int i = 2; i <= sqrtNumber; i++) {
         if (number % i == 0) {
-            return 0; 
+            return 0;
         }
     }
-    
-    return 1;  
+
+    return 1;
 }
 
 typedef struct {
@@ -35,9 +36,10 @@ typedef struct {
     void (*func)(void*);
     pthread_t thread;
     int active;
+    pthread_mutex_t activeMutex; // Added mutex to protect 'active' flag
 } ActiveObject;
 
-ActiveObject* getQueue(void* obj);
+ActiveObject* getActiveObject(void* obj);
 
 void initQueue(ThreadSafeQueue* queue) {
     queue->size = 0;
@@ -79,12 +81,21 @@ void* dequeue(ThreadSafeQueue* queue) {
     return item;
 }
 
-void* producer(void* arg) {
-    ActiveObject* activeObj = (ActiveObject*)arg;
-    ThreadSafeQueue* queue = activeObj->queue;
-    int N = activeObj->active;
+typedef struct {
+    ActiveObject* producerAO;
+    int active;
+    int seed;
+} ProducerArguments;
 
-    srand(time(NULL));
+void* producer(void* arg) {
+    ProducerArguments* args = (ProducerArguments*)arg;
+    ActiveObject* activeObj = args->producerAO;
+    ThreadSafeQueue* queue = activeObj->queue;
+    int N = args->active;
+    int seed = args->seed;
+
+    srand(seed);
+
     for (int i = 0; i < N; i++) {
         int* item = malloc(sizeof(int));
         *item = rand() % 900000 + 100000;  // Generate a random 6-digit number
@@ -97,125 +108,88 @@ void* producer(void* arg) {
 
 void processTask(void* task) {
     int* number = (int*)task;
-    printf("Received number: %d\n", *number);
-//    free(number);
+    printf("AO1: Generated number %d\n", *number);
+    ActiveObject* nextAO = getActiveObject(task);
+    enqueue(nextAO->queue, task);
 }
 
-ActiveObject* createActiveObject(ThreadSafeQueue* queue, void (*func)(void*)) {
-    ActiveObject* activeObj = malloc(sizeof(ActiveObject));
-    activeObj->queue = queue;
-    activeObj->func = func;
-    activeObj->active = 1;
-    pthread_create(&activeObj->thread, NULL, (void* (*)(void*))func, activeObj);
-    return activeObj;
-}
-
-ActiveObject* getQueue(void* obj) {
-    return (ActiveObject*)obj;
-}
-
-void stopActiveObject(ActiveObject* activeObj) {
-    activeObj->active = 0;
-    pthread_join(activeObj->thread, NULL);
-    free(activeObj);
-}
-
-void processTask1(void* task) {
-    int* number = (int*)task;
-    printf("AO1: Received number %d\n", *number);
-
-    ActiveObject* nextAO = (ActiveObject*)dequeue(getQueue(task));
-    enqueue(getQueue(nextAO), number + 11);
-
-    free(number);
-}
-
-ThreadSafeQueue* queue2;
 void processTask2(void* task) {
-    ActiveObject* activeObj = (ActiveObject*)task;
-    ThreadSafeQueue* queue = activeObj->queue;
-    ActiveObject* nextAO = (ActiveObject*)dequeue(queue);
-    int* number = (int*)dequeue(queue);
-
+    int* number = (int*)task;
     printf("AO2: Received number %d, isPrime: %s\n", *number, isPrime(*number) ? "true" : "false");
-    enqueue(queue, nextAO);
-    enqueue(queue2, number + 13);
-
-    free(number);
+    *number += 11;
+    printf("AO2: Updated number: %d\n", *number);
+    ActiveObject* nextAO = getActiveObject(task);
+    enqueue(nextAO->queue, task);
 }
-
 
 void processTask3(void* task) {
-    ActiveObject* activeObj = (ActiveObject*)task;
-    ThreadSafeQueue* queue = activeObj->queue;
-    ActiveObject* nextAO = (ActiveObject*)dequeue(queue);
-    int* number = (int*)dequeue(queue);
-
+    int* number = (int*)task;
     printf("AO3: Received number %d, isPrime: %s\n", *number, isPrime(*number) ? "true" : "false");
-    enqueue(getQueue(number - 2), nextAO);
-
-    free(number);
+    *number -= 13;
+    printf("AO3: Updated number: %d\n", *number);
+    ActiveObject* nextAO = getActiveObject(task);
+    enqueue(nextAO->queue, task);
 }
 
 void processTask4(void* task) {
     int* number = (int*)task;
     printf("AO4: Received number %d, new number: %d\n", *number, *number + 2);
-
-   free(number);
+    *number += 2;
+    ActiveObject* nextAO = getActiveObject(task);
+    enqueue(nextAO->queue, task);
 }
 
+ActiveObject* createActiveObject(ThreadSafeQueue* queue, void (*func)(void*)) {
+    ActiveObject* obj = malloc(sizeof(ActiveObject));
+    obj->queue = queue;
+    obj->func = func;
+    obj->active = 0;
+    pthread_mutex_init(&obj->activeMutex, NULL); // Initialize active mutex
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s N [seed]\n", argv[0]);
-        return 1;
+    pthread_create(&obj->thread, NULL, obj->func, obj);
+
+    return obj;
+}
+
+ActiveObject* getActiveObject(void* obj) {
+    return ((ActiveObject*)obj);
+}
+
+void stopActiveObject(ActiveObject* activeObj) {
+    pthread_mutex_lock(&activeObj->activeMutex); // Lock the active mutex
+    activeObj->active = 0;
+    pthread_mutex_unlock(&activeObj->activeMutex); // Unlock the active mutex
+    pthread_join(activeObj->thread, NULL);
+}
+
+int main(int argc, char** argv) {
+    int N = 2;
+    if (argc > 1) {
+        N = atoi(argv[1]);
     }
-
-    int N = atoi(argv[1]);
-    int seed = (argc > 2) ? atoi(argv[2]) : time(NULL);
-
-    if (N <= 0) {
-        printf("N must be a positive integer.\n");
-        return 1;
-    }
-
-    ThreadSafeQueue* queue1;
-    ThreadSafeQueue* queue2;
-    ThreadSafeQueue* queue3;
-    ThreadSafeQueue* queue4;
-
-    queue1 = malloc(sizeof(ThreadSafeQueue));
-    queue2 = malloc(sizeof(ThreadSafeQueue));
-    queue3 = malloc(sizeof(ThreadSafeQueue));
-    queue4 = malloc(sizeof(ThreadSafeQueue));
-
-    initQueue(queue1);
-    initQueue(queue2);
-    initQueue(queue3);
-    initQueue(queue4);
-
-    ActiveObject* AO1 = createActiveObject(queue1, processTask1);
-    ActiveObject* AO2 = createActiveObject(queue2, processTask2);
-   ActiveObject* AO3 = createActiveObject(queue3, processTask3);
-   ActiveObject* AO4 = createActiveObject(queue4, processTask4);
 
     printf("Starting the pipeline with N = %d\n", N);
 
-    pthread_t *producerThread;
-    ActiveObject producerAO;
-    producerAO.queue = queue1;
-    producerAO.active = N;
-    pthread_create(&producerThread, NULL, producer, (void*)&producerAO);
+    ThreadSafeQueue queue;
+    initQueue(&queue);
+
+    ActiveObject* ao1 = createActiveObject(&queue, processTask);
+    ActiveObject* ao2 = createActiveObject(&queue, processTask2);
+    ActiveObject* ao3 = createActiveObject(&queue, processTask3);
+    ActiveObject* ao4 = createActiveObject(&queue, processTask4);
+
+    ao1->active = N;
+
+    ProducerArguments producerArgs = { ao1, N, time(NULL) };
+    pthread_t producerThread;
+    pthread_create(&producerThread, NULL, producer, &producerArgs);
+
+    stopActiveObject(ao1);
+    stopActiveObject(ao2);
+    stopActiveObject(ao3);
+    stopActiveObject(ao4);
 
     pthread_join(producerThread, NULL);
-
-
-    stopActiveObject(AO1);
-  stopActiveObject(AO2);
-  stopActiveObject(AO3);
-  stopActiveObject(AO4);
-
-    printf("Pipeline completed.\n");
 
     return 0;
 }
